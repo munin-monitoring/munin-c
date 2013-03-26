a697a6ce70d3eb221f8e803ed96812946d30f6a3
Helmut Grohne <helmut@subdivi.de>
2013-03-26 17:05:16 +0100

    also update the license crap

--- plugins-c/memory.c
+++ plugins-c/memory.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2013 Steve Schnepp <steve.schnepp@pwkf.org> - All rights reserved.
+ * Copyright (C) 2013 Helmut Grohne <helmut@subdivi.de> - All rights reserved.
  *
  * This copyrighted material is made available to anyone wishing to use,
  * modify, copy, or redistribute it subject to the terms and conditions
