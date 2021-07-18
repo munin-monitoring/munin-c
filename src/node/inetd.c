munin-monitoring/munin-c

commit fe9b654fd44b78fe1aef099fda3c60808a2f9227

Revert "inetd: using posix_spawn() instead of fork/exec"
This reverts commit 1722cb36d65b42199aed4d58da27ec1520249f55.

src/node/inetd.c
@@ -18,7 +18,6 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
-#include <spawn.h>
 
 #if !(defined(HAVE_WORKING_VFORK) || defined(S_SPLINT_S))
 #define vfork fork
@@ -83,20 +82,18 @@ int main(int argc, char *argv[])
 	/* We do *not* care about childs */
 	signal(SIGCHLD, SIG_IGN);
 
-	while ((sock_accept = accept(sock_listen, NULL, NULL)) != -1) {
-		posix_spawn_file_actions_t action;
-
-		posix_spawn_file_actions_init(&action);
-		posix_spawn_file_actions_addclose(&action, sock_listen);
-		posix_spawn_file_actions_adddup2(&action, sock_accept, 0);
-		posix_spawn_file_actions_adddup2(&action, sock_accept, 1);
-		posix_spawn_file_actions_addclose(&action, sock_accept);
-
-		if (0 == posix_spawnp(&pid, argv[2],
-				      &action, NULL, argv + 3, NULL)) {
+	while((sock_accept = accept(sock_listen, NULL, NULL)) != -1) {
+		if(0 == (pid = vfork())) {
+			close(sock_listen);
+			dup2(sock_accept, 0);
+			dup2(sock_accept, 1);
 			close(sock_accept);
+			execvp(argv[2], argv + 3);
+			/* according to vfork(2) we must use _exit */
+			_exit(1);
 		} else {
 			perror("vfork failed in " __FILE__);
+			close(sock_accept);
 			close(sock_listen);
 			return 1;
 		}

src/node/node.c
@@ -26,7 +26,6 @@
 #include <grp.h>
 #include <fnmatch.h>
 #include <ctype.h>
-#include <spawn.h>
 
 #ifndef HOST_NAME_MAX
 #define HOST_NAME_MAX 256
@@ -649,7 +648,7 @@ static int handle_connection()
 				continue;
 			}
 
-			/* Using posix_spawnp() here instead of fork() since we will
+			/* Using fork() here instead of vork() since we will
 			 * do a little more than a mere exec --> setenvvars_conf() */
 			pid = fork();
 

commit a4524a84c437b59c3c8ed70011428dfa20e0eeec

inetd: consider only negative vfork() return as failure
Fixes: fe9b654fd4 ("Revert "inetd: using posix_spawn() instead of fork/exec"")
Signed-off-by: Matt Merhar <mattmerhar@protonmail.com>

src/node/inetd.c
@@ -92,10 +92,13 @@ int main(int argc, char *argv[])
 			/* according to vfork(2) we must use _exit */
 			_exit(1);
 		} else {
-			perror("vfork failed in " __FILE__);
 			close(sock_accept);
-			close(sock_listen);
-			return 1;
+
+			if (pid < 0) {
+				perror("vfork failed in " __FILE__);
+				close(sock_listen);
+				return 1;
+			}
 		}
 	}
 	perror("accept failed in " __FILE__);