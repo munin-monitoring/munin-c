8966ea65a866bbb7d498c9b02aa5302894876c3e
Steve Schnepp <steve.schnepp@pwkf.org>
2021-07-22 18:32:18 +0200

    p/external_: fix typo in copyright email address

--- src/plugins/p/external_.c
+++ src/plugins/p/external_.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Steve Schnepp <steve.schnepp@pwkg.org> - All rights reserved.
+ * Copyright (C) 2013 Steve Schnepp <steve.schnepp@pwkf.org> - All rights reserved.
  *
  * This copyrighted material is made available to anyone wishing to use,
  * modify, copy, or redistribute it subject to the terms and conditions
